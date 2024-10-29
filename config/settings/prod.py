424e562e98fd436b78f1ad6084456f545668a6b0
jaekyung123 <jaekyung123@gmail.com>
2024-10-29 15:16:17 +0900

    debug

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -1,6 +1,6 @@
 from .base import *
 
-ALLOWED_HOST = ['15.165.163.173']
-STATIC_ROOT = BASE_DIR / 'static/'
+ALLOWED_HOSTS = ['15.165.163.173']
+STATIC_ROOT = BASE_DIR / 'pybo/static/'
 STATICFILES_DIRS = []
 DEBUG = False
